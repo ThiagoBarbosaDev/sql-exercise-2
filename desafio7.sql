14d4fb9d431d01799ffeacaba838e75942ed3659
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-09-24 19:10:51 -0300

    fix: desafio7.sql

--- desafio7.sql
+++ desafio7.sql
@@ -1,9 +1,12 @@
-SELECT
-	A.artista AS artista,
-	AL.album AS album,
-	COUNT(SA.usuario_id) AS seguidores
-FROM SpotifyClone.album AL
-	INNER JOIN SpotifyClone.artista A ON AL.artista_id = A.id
-	INNER JOIN SpotifyClone.seguindo_artistas SA ON AL.artista_id = SA.artista_id
-GROUP BY AL.id
-ORDER BY seguidores desc, artista;
+SELECT 
+  A.artista,
+  AL.album,
+  COUNT(AL.Album) AS seguidores
+FROM SpotifyClone.album as AL
+INNER JOIN SpotifyClone.artista A ON AL.artista_id = A.id
+INNER JOIN SpotifyClone.seguindo_artistas SA ON SA.artista_id = AL.artista_id
+GROUP BY album
+ORDER BY 
+	seguidores desc, 
+  artista,
+  album;
